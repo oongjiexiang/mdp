691063bcb38e42fa4c11cf0998ef3d3b434e7046
Oong Jie Xiang <oongjiexiang@yahoo.com>
2021-08-25 13:16:18 +0800

    debugged calc_path

--- Algorithms/exploration_week8/hamiltonian_brute.c
+++ Algorithms/exploration_week8/hamiltonian_brute.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 // Data structures
@@ -23,12 +24,15 @@ Obstacle* heuristic_greedy(Obstacle *obstacles, int n);
 Obstacle* heuristic_greedy_enhanced(Obstacle *obstacles, int n);
 Obstacle* heuristic_exhaustive(Obstacle *obstacles, int n);
 Obstacle* heuristic_euclidean(Obstacle *obstacles, int n);
+double calc_path(int* path, int n, double* dist);
 double calc_euclidean(Obstacle o1, Obstacle o2);
+void output_result(Obstacle* results, int n_obs);
 void permutation(int* path_order, int l, int r, double* min_dist, int* min_path);
 void swap(int *a, int *b);
+void swap_obs(Obstacle *a, Obstacle *b);
 
 // Input: to delete when STM defines IO input
-Obstacle* input[7] = {
+Obstacle input[7] = {
     {1, 40, 30},
     {2, 45, 100},
     {3, 90, 90},
@@ -41,11 +45,12 @@ Obstacle* input[7] = {
 int main(){
     // initialise
     int n_obs = 0;
+    Obstacle *results;
     start.id = 0; start.x_coor = ROBOT_LENGTH/2; start.y_coor = ROBOT_LENGTH/2;
 
     // start exploration procedure
     Obstacle *obstacles = populate_obstacles(&n_obs);
-    Obstacle *results = shortest_path(obstacles, n_obs);
+    results = shortest_path(obstacles, n_obs);
     output_result(results, n_obs);
     return 0;
 }
@@ -84,7 +89,7 @@ Obstacle* heuristic_euclidean(Obstacle *obstacles, int n){
 
     // calculate distance adjacency matrix
     int n_locations = n+1;
-    float* dist = (float*)malloc(sizeof(float)*n_locations*n_locations);
+    dist = (double*)malloc(sizeof(double)*n_locations*n_locations);
     for(i = 0; i < n_locations; i++){
         dist[obstacles[i].id*n_locations + start.id] = calc_euclidean(obstacles[i], start);
         dist[start.id*n_locations + obstacles[i].id] = dist[obstacles[i].id*n_locations + start.id];
@@ -93,9 +98,16 @@ Obstacle* heuristic_euclidean(Obstacle *obstacles, int n){
             dist[obstacles[j].id*n_locations + obstacles[i].id] = dist[obstacles[i].id*n_locations + obstacles[j].id];
         }
     }
-
+    //delete_s
+    for(i = 0; i < n_locations; i++){
+        for(j = 0; j < n_locations; j++){
+            printf("%d|%.0f\t", i*n_locations + j, dist[i*n_locations + j]);
+        }
+        printf("\n");
+    }
+    //delete_e
     // calculate shortest cycle
-    int* min_path = NULL;
+    int* min_path = malloc(sizeof(int)*n);
     double min_dist = -1;
     int* order = malloc(sizeof(int)*n);
     for(i = 0; i < n; i++){
@@ -103,6 +115,7 @@ Obstacle* heuristic_euclidean(Obstacle *obstacles, int n){
     }
     permutation(order, 1, n, &min_dist, min_path);
 
+    printf("Freed\n");
     free(dist);
     free(order);
 
@@ -111,15 +124,15 @@ Obstacle* heuristic_euclidean(Obstacle *obstacles, int n){
     for(i = 0; i < n; i++){
         printf("%d ", min_path[i]);
     }
-    puts('\n');
+    putchar('\n');
 
     // returning Obstacle objects
     Obstacle* tmp = obstacles;
-    int index, j = 0;
+    int index = 0;
     while(tmp != NULL){
         for(j = index; j < n; j++){
             if(tmp->id == min_path[j]){
-                swap(tmp, obstacles + j);
+                swap_obs(tmp, obstacles + j);
                 tmp+=1;
                 index+=1;
                 break;
@@ -139,8 +152,11 @@ void permutation(int* path_order, int l, int r, double* min_dist, int* min_path)
     int i;
     double distance_buffer;
     if(l == r){
-        if(calc_path(path_order, r, dist) < *min_dist){
-            *min_path = path_order;
+        for(i = 0; i < r; i++) printf("%d ", path_order[i]);        // delete
+        if(calc_path(path_order, r+1, dist) < *min_dist){
+            for(i = 0; i < r; i++){
+                min_path[i] = path_order[i];
+            }
             *min_dist = distance_buffer;
         }
         return;
@@ -153,21 +169,30 @@ void permutation(int* path_order, int l, int r, double* min_dist, int* min_path)
 }
 
 // Assumption: 5-8 targets, never less than 3
-double calc_path(int* path, int n, double* dist){
+double calc_path(int* path, int n_location, double* dist){
     int i;
     double path_length = 0;
-    for(i = 0; i < n - 1; i++){
-        path_length+=dist[path[i]*n + path[i+1]];
+    for(i = 0; i < n_location - 1; i++){
+        printf("%d-%d: %d\n", path[i], path[i+1], path[i]*n_location + path[i+1]);
+        printf("%.2f\n", dist[path[i]*n_location + path[i+1]]);
+        path_length+=dist[path[i]*n_location + path[i+1]];
     }
 
     // add the distance from START point to first target, and from last target back to START
-    path_length = path_length + dist[path[n-1]*n + 0] + dist[0*n + path[0]];
+    path_length = path_length + dist[path[n_location-1]*n_location + 0] + dist[0*n_location + path[0]];
     return path_length;
 }
 
 // Done
 void swap(int *a, int *b){
     int *temp;
+    temp = b;
+    *a = *b;
+    *b = *temp;
+}
+
+void swap_obs(Obstacle *a, Obstacle *b){
+    Obstacle *temp;
     temp = b;
     *a = *b;
     *b = *temp;
