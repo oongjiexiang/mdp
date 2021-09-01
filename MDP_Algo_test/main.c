71ef086bbe49e7db0a9f34874310985bb822a815
ngzhiyu97 <62195263+ngzhiyu97@users.noreply.github.com>
2021-09-01 15:43:42 +0800

    updated a star search and added obstacles
    a star search still does not take into obstacles yet, will update again tonight or tomorrow

--- MDP_Algo_test/main.c
+++ MDP_Algo_test/main.c
@@ -8,8 +8,8 @@
 //double UNIT_LENGTH = 10;     // cm
 //double ROBOT_LENGTH = 30;
 //double OBSTACLE_BOUNDARY = 15; //how to do this one?
-int unit_length,robot_length,obstacle_boundary,row_count,column_count,total_grid_count;
-
+int row_count,column_count,total_grid_count;
+double unit_length, obstacle_size,robot_length,obstacle_boundary;
 
 /*
     theta refers to the direction
@@ -72,38 +72,40 @@ struct Queue_Node{
 //----done----
 double calculate_g_cost(struct Vertex* prev_vertex, struct Vertex* current_vertex);
 void update_h_cost(struct Vertex* vertex_array, struct Vertex* src_vertex, struct Vertex* target_vertex, int total_grid_count);//caculates the hcost, gcost+heuristic value, distance from source and estimated distance from target, distance from source
+void print_path(struct Vertex* src_vertex, struct Vertex* target_vertex);
+
 //----not done----
+struct Vertex* a_star_search(struct Vertex* vertex_array, struct Vertex* adj_matrix[total_grid_count][total_grid_count], struct Vertex* src_vertex, struct Vertex* target_vertex, int total_grid_count);
 double* trip_planning(struct Vertex* obstacle_array, struct Vertex* vertex_array, struct Robot* robot);//a*search with obstacle avoidance, returns the path and direction(theta)
 bool obstacle_avoidance(struct Vertex* obstacle_array, struct Robot* robot);//obstacle avoidance checking and redirection, returns true if obstacle found, false if no obstacle
 int check_turn_condition(struct Vertex* start_loc, struct Vertex* end_loc);//calculates which of the 3 scenarios the source and vertex falls under and returns 1, 2, or 3
-struct Vertex* a_star_search(struct Vertex* vertex_array, struct Vertex* adj_matrix[total_grid_count][total_grid_count], struct Vertex* src_vertex, struct Vertex* target_vertex, int total_grid_count);
 void update_robot(struct Robot* robot);//update the robot's coordinates
-
 //----vertex functions----
-//----done----
 void initialize_vertex(struct Vertex* vertex_array,int row_count, int column_count,int unit_length,int total_count);//initialize all the vertex variables
 void reset_vertex(struct Vertex* vertex_array,int row_count, int column_count,int unit_length,int total_count);//reset the cost, obstacle, border, visited, and prev_vertex for all vertices, use this every time after 1 path has been found
 void generate_adj_matrix(struct Vertex* adj_matrix[total_grid_count][total_grid_count],struct Vertex* vertex_array, int row_count, int column_count, int total_count);
-//----not done----
-void add_obstacle(struct Vertex* vertex_array, struct Obstacle* obstacle_array, int no_of_obstacles, int unit_length,int border_length, int total_count);//add obstacles and borders
-
+struct Vertex* find_vertex(struct Vertex* vertex_array, double x_coor, double y_coor, int total_count);//assumes the x and y coordinates given refer to the middle of the object
+void add_obstacle(struct Vertex* vertex_array, struct Obstacle* obstacle_array, int no_of_obstacles, double obstacle_length,double boundary_size, int total_count);//add obstacles and borders
 //----queue functions----
 void create_empty_queue(struct Sorted_Queue* q);
 void enqueue(struct Sorted_Queue* q, struct Vertex* vertex);
 struct Vertex* dequeue(struct Sorted_Queue* q);
 void sort_queue(struct Sorted_Queue* q);
+//----test functions----
+void test_queue();
 
 int main()
 {
     int i, src,target;
     unit_length=10;//set to 5 so that obstacle boundary can be calculated more easily
     robot_length=30;
-    obstacle_boundary=15;// how to check for halfgrid? make each grid 5 wide?
-    row_count=5;
-    column_count=5;
+    obstacle_boundary=15;// + to each side of the obstacle
+    obstacle_size=10;//total length of an obstacle, the obstacle is a square
+    row_count=20;
+    column_count=20;
     total_grid_count = row_count*column_count;
     src=0;
-    target=9;
+    target=46;
     //----test vertex fucntions----
     struct Vertex* vertex_array= (struct Vertex*)malloc(total_grid_count*sizeof(struct Vertex));
     initialize_vertex(vertex_array,row_count,column_count,unit_length,total_grid_count);
@@ -111,57 +113,30 @@ int main()
             printf("%d\n",i);
         printf("vertex values: %d, %d, %d, %f, %f, %f, %f\n",vertex_array[i].id,vertex_array[i].g_cost,vertex_array[i].h_cost,vertex_array[i].x_left,vertex_array[i].x_right,vertex_array[i].y_high,vertex_array[i].y_low);
     }
-
     struct Vertex* adj_matrix[total_grid_count][total_grid_count];
+    struct Obstacle obstacle_array[1] = {{0,55 ,15}};
+    add_obstacle(vertex_array, obstacle_array,1,obstacle_size,obstacle_boundary,total_grid_count);
 
+    printf("index %d, obstacle = %d\n",vertex_array[25].id,vertex_array[25].is_obstacle);
     generate_adj_matrix(adj_matrix,vertex_array,row_count,column_count,total_grid_count);
     update_h_cost(vertex_array,&vertex_array[src],&vertex_array[target],total_grid_count);
-    struct Vertex* vptr = a_star_search(vertex_array,adj_matrix,&vertex_array[src],&vertex_array[target],total_grid_count);
-
-    for(i=0;i<25;i++){
+    a_star_search(vertex_array,adj_matrix,&vertex_array[src],&vertex_array[target],total_grid_count);
+    for(i=0;i<40;i++){
         if(vertex_array[i].prev_vertex!=NULL)
         printf("i: %d prev_vertex: %d\n",i,vertex_array[i].prev_vertex->id);
     }
-
     //----end of test vertex----
 
-    /*
-    //---test queue functions---
-    struct Vertex qv[5]={
-    { 0,10},{ 1,15},{ 2,5},{ 3,25},{ 4,20}
-    };
-    struct Vertex* qptr=(struct Vertex*)malloc(sizeof(struct Vertex*));
-    struct Sorted_Queue* q = (struct Sorted_Queue*)malloc(sizeof(struct Sorted_Queue));
-    create_empty_queue(q);
-    for(i=0;i<5;i++){
-        qptr=&qv[i];
-        enqueue(q,qptr);
-    }
-    struct Queue_Node* curr=q->head;
-    for(i=0;i<q->current_size;i++){
-        printf("%d ",curr->q_vertex->g_cost);
-        curr=curr->next;
-    }
-    printf("\n");
-    sort_queue(q);
-    curr=q->head;
-    for(i=0;i<q->current_size;i++){
-        printf("%d ",curr->q_vertex->g_cost);
-        curr=curr->next;
-    }
-    free(q);
-    //----end of test queue----
-    */
     free(vertex_array);
-
+    //test_queue();
     return 0;
 }
 
 //----Search functions
+//add obstacle avoidance and stop search once the goal has been found
 struct Vertex* a_star_search(struct Vertex* vertex_array, struct Vertex* adj_matrix[total_grid_count][total_grid_count], struct Vertex* src_vertex, struct Vertex* target_vertex, int total_grid_count){
     int i, temp_g_cost;
     struct Vertex* curr_vertex;
-    struct Vertex* print_vertex;
     struct Vertex* neighbour_vertex;
     struct Sorted_Queue* q = (struct Sorted_Queue*)malloc(sizeof(struct Sorted_Queue*));
     create_empty_queue(q);
@@ -173,6 +148,12 @@ struct Vertex* a_star_search(struct Vertex* vertex_array, struct Vertex* adj_mat
         //visit head of sorted q
         curr_vertex=dequeue(q);
         curr_vertex->visited=true;
+        if(curr_vertex->id==target_vertex->id){
+            print_path(src_vertex,target_vertex);
+            free(q);
+            q=NULL;
+            return target_vertex;
+        }
         //for each neighbour in the adjacency matrix
         for(i=0;i<total_grid_count;i++){
             if(adj_matrix[curr_vertex->id][i]!=NULL){
@@ -192,16 +173,20 @@ struct Vertex* a_star_search(struct Vertex* vertex_array, struct Vertex* adj_mat
         //move the lowest f_cost vertex to front of q
         sort_queue(q);
     }
-    //print the path (backwards)
+    print_path(src_vertex,target_vertex);
+    free(q);
+    q=NULL;
+    return target_vertex;
+}
+//prints the path from a star search backwards, to implement forward, save the id into an array starting from the last element or use a stack
+void print_path(struct Vertex* src_vertex, struct Vertex* target_vertex){
+    struct Vertex* print_vertex;
     print_vertex=target_vertex;
     printf("path to reach vertex %d from source %d\n",target_vertex->id, src_vertex->id);
     while(print_vertex->id!=src_vertex->id){
         printf(" vertex %d\n",print_vertex->prev_vertex->id);
         print_vertex=print_vertex->prev_vertex;
     }
-    free(q);
-    q=NULL;
-    return target_vertex;
 }
 //updates the h_cost for every vertex using Euclidean distance from previous vertex
 double calculate_g_cost(struct Vertex* prev_vertex, struct Vertex* current_vertex){
@@ -236,10 +221,10 @@ void initialize_vertex(struct Vertex* vertex_array,int row_count, int column_cou
             vertex_array[current_index].id = current_index;
             vertex_array[current_index].g_cost = start_cost;
             vertex_array[current_index].h_cost = start_cost;
-            vertex_array[current_index].x_right =j*unit_length;
-            vertex_array[current_index].x_left =((j+1)*unit_length)-1;
-            vertex_array[current_index].y_high =i*unit_length;
-            vertex_array[current_index].y_low =((i+1)*unit_length)-1;
+            vertex_array[current_index].x_right =((j+1)*unit_length)-1;
+            vertex_array[current_index].x_left =j*unit_length;
+            vertex_array[current_index].y_high =((i+1)*unit_length)-1;
+            vertex_array[current_index].y_low =i*unit_length;
             vertex_array[current_index].is_obstacle = false;
             vertex_array[current_index].is_border = false;
             vertex_array[current_index].visited = false;
@@ -348,32 +333,60 @@ void generate_adj_matrix(struct Vertex* adj_matrix[total_grid_count][total_grid_
         }
     }
 }
-/*
-//adds an obstacles and the borders of the obstacles into the grid
-//not complete
-void add_obstacle(struct Vertex* vertex_array, struct Obstacle* obstacle_array, int no_of_obstacles, int unit_length,int border_length, int total_count){
-    int x_pos, y_pos, i, j, k, l;
-    //find obstacle location on vertex array
+
+//adds an obstacles and the borders of the obstacles into the grid given the array of obstacles
+void add_obstacle(struct Vertex* vertex_array, struct Obstacle* obstacle_array, int no_of_obstacles, double obstacle_length,double boundary_size, int total_count){
+    int i,j;
+    double obstacle_x_left, obstacle_x_right, obstacle_y_high, obstacle_y_low, half_obstacle_length, border_x_left, border_x_right, border_y_high, border_y_low;
+    half_obstacle_length=obstacle_length/2;
+
+    //loop through the obstacle array
     for(i=0;i<no_of_obstacles;i++){
-        x_pos=obstacle_array[i].x_coor;
-        y_pos=obstacle_array[i].y_coor;
+        //find the 4 points of the obstacle
+        obstacle_x_left = obstacle_array[i].x_coor-half_obstacle_length;
+        obstacle_x_right = obstacle_array[i].x_coor+half_obstacle_length;
+        obstacle_y_high = obstacle_array[i].y_coor+half_obstacle_length;
+        obstacle_y_low = obstacle_array[i].y_coor-half_obstacle_length;
+        printf("obs x and y : %f, %f, %f, %f",obstacle_x_left,obstacle_x_right,obstacle_y_high,obstacle_y_low);
+
+        //find the 4 points of the border
+        border_x_left = obstacle_array[i].x_coor-boundary_size;
+        border_x_right = obstacle_array[i].x_coor+boundary_size;
+        border_y_high = obstacle_array[i].y_coor+boundary_size;
+        border_y_low = obstacle_array[i].y_coor-boundary_size;
+        //check every vertex
         for(j=0;j<total_count;j++){
-            //if x_pos and y_pos are within the x and y coordinates of a vertex, that vertex is set to an obstacle, assuming each grid is 10 wide
-            //set the corresponding vertex is_obstacle to true
-            if(x_pos>=vertex_array[j].x_left && x_pos<=vertex_array.x_right && y_pos>=vertex_array[j].y_low && y_pos<=vertex_array[j].y_high){
-                vertex_array[j].is_obstacle=true;
-                break;
+            // check if a vertex overlaps with an obstacle, if it does set it as an obstacle
+            // x1-------x2 x1<=x4, x2>=x3
+            // x3-------x4
+            if(vertex_array[j].x_left <= obstacle_x_right && vertex_array[j].x_right >=obstacle_x_left && vertex_array[j].y_low <= obstacle_y_high && vertex_array[j].y_high >=obstacle_y_low){
+                vertex_array[j].is_obstacle = true;
+                printf("obstacle set\n");
+            }
+            //if a vertex falls within the x and y coordinates of the border and the vertex is not an obstacle, set it as the border
+            if(vertex_array[j].x_left <= border_x_right && vertex_array[j].x_right >=border_x_left && vertex_array[j].y_low <= border_y_high && vertex_array[j].y_high >=border_y_low && vertex_array[j].is_obstacle==false){
+                vertex_array[j].is_border=true;
+                printf("border set\n");
             }
         }
-    //set the neighbours to border
-    //2 for loops
-    //-2 to +2 -2,-1,0,1,2 for row & col
-    //
-
     }
-
 }
-*/
+
+//search for a vertex given the x and y coordinates and returns a pointer to the vertex
+struct Vertex* find_vertex(struct Vertex* vertex_array, double x_coor, double y_coor, int total_count){
+    int i;
+    struct Vertex* result=NULL;
+    for(i=0;i<total_count;i++){
+        //check if x and y coordinates are within the vertex x and y coordinates
+        if(x_coor >= vertex_array[i].x_left && x_coor <=vertex_array[i].x_right && y_coor >= vertex_array[i].y_low && y_coor <= vertex_array[i].y_high){
+            result = &vertex_array[i];
+        }
+    }
+    if(result==NULL){
+        printf("vertex could not be found.");
+    }
+    return result;
+};
 
 //----Queue functions----
  void create_empty_queue(struct Sorted_Queue* q){
@@ -454,3 +467,31 @@ void sort_queue(struct Sorted_Queue* q){
     }
 }
 
+//----test functions----
+void test_queue(){
+    int i;
+    struct Vertex qv[5]={
+    { 0,10},{ 1,15},{ 2,5},{ 3,25},{ 4,20}
+    };
+    struct Vertex* qptr;
+    struct Sorted_Queue* q = (struct Sorted_Queue*)malloc(sizeof(struct Sorted_Queue));
+    create_empty_queue(q);
+    for(i=0;i<5;i++){
+        qptr=&qv[i];
+        enqueue(q,qptr);
+    }
+    struct Queue_Node* curr=q->head;
+    for(i=0;i<q->current_size;i++){
+        printf("%d ",curr->q_vertex->g_cost);
+        curr=curr->next;
+    }
+    printf("\n");
+    sort_queue(q);
+    curr=q->head;
+    for(i=0;i<q->current_size;i++){
+        printf("%d ",curr->q_vertex->g_cost);
+        curr=curr->next;
+    }
+    free(q);
+    q=NULL;
+}
