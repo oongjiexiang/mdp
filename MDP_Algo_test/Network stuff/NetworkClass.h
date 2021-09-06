d5053e1d65e2da06d6d01af43d4ee43060fe07fb
ngzhiyu97 <62195263+ngzhiyu97@users.noreply.github.com>
2021-09-06 22:11:17 +0800

    Updated NetworkClass.cpp and NetworkClass.h
    Functions not tested yet!

---
+++ MDP_Algo_test/Network stuff/NetworkClass.h
@@ -0,0 +1,27 @@
+#define WIN32_LEAN_AND_MEAN
+//makes it so that the code compiled only works for windows vista and above(required in order to use inet_pton function)
+#define NTDDI_VERSION NTDDI_VISTA
+#define WINVER _WIN32_WINNT_VISTA
+#define _WIN32_WINNT _WIN32_WINNT_VISTA
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+
+class Network{
+    public:
+    std::string serverIP; //ip address of the server
+    int portNumber; //listening port on the server
+    int checker;
+    std::string message;//u8 encodes the string us utf-8 format
+    int bufferLength; //change this later
+    Network(std::string ipAddr, int portNo);
+    int initializeConnection();
+    std::string encodeMessage(int targetDevice, std::string unformattedMessage);
+    std::string decodeMessage(std::string receivedMessage);
+    int sendMessage(std::string formattedMessage);
+    void endConnection();
+};
