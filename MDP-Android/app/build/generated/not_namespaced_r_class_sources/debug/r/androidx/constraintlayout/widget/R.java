c7557ae1f13ca0dd9ab8084e386eb30354ec2831
Loh Xin Yi <lohxinyi135@gmail.com>
2021-09-08 23:40:42 +0800

    Revert "Revert "Added Bluetooth Chat UI & Update controller""
    This reverts commit 66d0e5f493218f80aed2e686ec2cd8c0f8e3abb3.

--- MDP-Android/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ MDP-Android/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f090053;
-        public static final int end = 0x7f090086;
-        public static final int gone = 0x7f0900a8;
-        public static final int invisible = 0x7f0900b5;
-        public static final int left = 0x7f0900ba;
-        public static final int packed = 0x7f0900df;
-        public static final int parent = 0x7f0900e3;
-        public static final int percent = 0x7f0900e6;
-        public static final int right = 0x7f0900ef;
-        public static final int spread = 0x7f09011d;
-        public static final int spread_inside = 0x7f09011e;
-        public static final int start = 0x7f090123;
-        public static final int top = 0x7f090142;
-        public static final int wrap = 0x7f09015e;
+        public static final int bottom = 0x7f090054;
+        public static final int end = 0x7f090088;
+        public static final int gone = 0x7f0900aa;
+        public static final int invisible = 0x7f0900b7;
+        public static final int left = 0x7f0900bc;
+        public static final int packed = 0x7f0900e1;
+        public static final int parent = 0x7f0900e5;
+        public static final int percent = 0x7f0900e8;
+        public static final int right = 0x7f0900f1;
+        public static final int spread = 0x7f09011f;
+        public static final int spread_inside = 0x7f090120;
+        public static final int start = 0x7f090125;
+        public static final int top = 0x7f090144;
+        public static final int wrap = 0x7f090160;
     }
     public static final class styleable {
         private styleable() {}
--- MDP-Android/app/src/main/java/com/example/mdp_android_grp15/MainActivity.java
+++ MDP-Android/app/src/main/java/com/example/mdp_android_grp15/MainActivity.java
@@ -22,6 +22,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.viewpager.widget.ViewPager;
 
+import com.example.mdp_android_grp15.ui.main.BluetoothChat;
 import com.example.mdp_android_grp15.ui.main.BluetoothConnectionService;
 import com.example.mdp_android_grp15.ui.main.BluetoothPopUp;
 import com.example.mdp_android_grp15.ui.main.CommsFragment;
@@ -49,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static GridMap gridMap;
     static TextView xAxisTextView, yAxisTextView, directionAxisTextView;
-    static TextView robotStatusTextView;
+    static TextView robotStatusTextView, bluetoothStatus;
     static Button f1, f2;
     static Button upBtn, downBtn, leftBtn, rightBtn;
     Button reconfigure;
@@ -110,6 +111,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(popup);
             }
         });
+        Button chatButton = findViewById(R.id.chatButton);
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chat = new Intent(MainActivity.this, BluetoothChat.class);
+                startActivity(chat);
+            }
+        });
+
+        // Bluetooth Status
+        bluetoothStatus = findViewById(R.id.bluetoothStatus);
+        //bluetoothStatus.setText("Not Connected");
 
 
         // Map
@@ -212,6 +225,8 @@ public class MainActivity extends AppCompatActivity {
     public static Button getDownBtn() { return downBtn; }
     public static Button getLeftBtn() { return leftBtn; }
     public static Button getRightBtn() { return rightBtn; }
+
+    public static TextView getbluetoothStatus() { return bluetoothStatus; }
 
     public static void sharedPreferences() {
         sharedPreferences = MainActivity.getSharedPreferences(MainActivity.context);
