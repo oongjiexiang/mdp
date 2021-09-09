7abbc96e9f09aa58e402cf6c7528c3410286e313
Oong Jie Xiang <oongjiexiang@yahoo.com>
2021-09-09 13:14:07 +0800

    touch up of asearch header file before building Hamiltonian path

---
+++ MDP_Algo_test/asearch.h
@@ -0,0 +1,37 @@
+#ifndef ASEARCH_H
+#define ASEARCH_H
+
+#include "config.h"
+#include "math.h"
+#include <vector>
+#include <iostream>
+#include <tuple>
+
+// Creating a shortcut for tuple<int, Vertex> type
+typedef tuple<double, int, int> Tuple;
+
+class aStar{
+    Map* grid;
+
+    // check whether cell is not blocked
+    bool isUnBlocked(const Vertex& vertex); 
+    
+    bool isDestination(const Vertex& position, const Vertex& dest);
+    
+    // A Utility Function to calculate the 'h' heuristics.
+    double calculateHValue(const Vertex& src, const Vertex& dest);  // tochange
+    
+    // Encapsulate g cost calculation
+    double calculateGValue(Vertex& cur);
+    
+    // A Utility Function to trace the path from the source to destination
+    void tracePath(const vector<vector<Vertex>>& cellDetails, Vertex dest);
+    
+    public:
+        aStar();
+        aStar(vector<vector<int>> fullMap);
+        void search(Vertex& src, Vertex& dest);
+};
+
+#endif
+
